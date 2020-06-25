8ee291c2f1f885a7e45020bfc8442bc03d2a629e
antatura <47680985+antatura@users.noreply.github.com>
2020-06-25 23:42:20 +0800

    Update Create a file of the specified size.ps1

--- ScriptBlock/Create a file of the specified size.ps1
+++ ScriptBlock/Create a file of the specified size.ps1
@@ -1,9 +1,13 @@
 function New-EmptyFile
 {
-   param( [string]$FilePath,[double]$Size )
+    param( [string]$FilePath,[double]$Size )
  
-   $file = [System.IO.File]::Create($FilePath)
-   $file.SetLength($Size)
-   $file.Close()
-   Get-Item $file.Name
+    $file = [System.IO.File]::Create($FilePath)
+    $file.SetLength($Size)
+    $file.Close()
+    Get-Item $file.Name
 }
+
+PS> New-EmptyFile -FilePath c:\temp\test.txt -Size 20mb
+
+# https://www.powershellmagazine.com/2012/11/22/pstip-create-a-file-of-the-specified-size/
