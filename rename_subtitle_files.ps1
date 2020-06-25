f8aebd69b81db0a318c147a5797abaf0e1473f7d
antatura <47680985+antatura@users.noreply.github.com>
2020-06-25 23:58:22 +0800

    Update rename_subtitle_files.ps1

--- rename_subtitle_files.ps1
+++ rename_subtitle_files.ps1
@@ -1,8 +1,12 @@
 Write-Host 'Rename subtitle files with video basename' -ForegroundColor Yellow
+
 $VE = '.'+(Read-Host -Prompt 'Video Extension')
 $SE = '.'+(Read-Host -Prompt 'Subtitle Extension')
+
 $VE_BaseName = (Get-ChildItem *$VE).BaseName
+
 for ($X=0; $X -lt $VE_BaseName.Length; $X++) {
     (Get-ChildItem *$SE)[$X] | Rename-Item -NewName ($VE_BaseName[$X]+$SE)
 }
+
 Get-ChildItem
