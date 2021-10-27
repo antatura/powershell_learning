da1a67599fd0536fed0073d336d23adc8db7733b
antatura <47680985+antatura@users.noreply.github.com>
2021-10-28 02:06:00 +0800

    Add files via upload

---
+++ 按日期创建文件夹.ps1
@@ -0,0 +1,10 @@
+﻿$date = Get-Date 2021.10.01
+
+for($E=0; $date -lt (Get-Date 2021.12.31); $E++){
+    $date = (Get-Date 2021.10.01).AddDays($E)
+    $yea = '{0:d4}'-f $date.Year
+    $mon = '{0:d2}'-f $date.Month
+    $day = '{0:d2}'-f $date.Day
+    $nam = $yea+'.'+$mon+'.'+$day
+    New-Item -ItemType Directory .\$yea'年'\$mon'月'\$nam
+}
