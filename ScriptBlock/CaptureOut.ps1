b2a77bbe4cadf82606a1d34bb92ed7d422e62827
antatura <47680985+antatura@users.noreply.github.com>
2022-06-15 15:17:01 +0800

    Add files via upload

--- ScriptBlock/CaptureOut.ps1
+++ ScriptBlock/CaptureOut.ps1
@@ -1,35 +1,42 @@
 ﻿
 $video = $args[0]
 
-$number = [int](Read-Host "Number of Captures")
+$W = [int](Read-Host "[int]Pixel width of the final tiles")    # 最终合成图的像素宽度
 
-$duration = [double](ffprobe -v 16 -show_entries format=duration -of csv=p=0 $video)
+$X = [int](Read-Host "[int]X-COLUMNS")    # 横向贴片数量
 
-for ($E=1; $E -le $number; $E++) 
+$Y = [int](Read-Host "[int]Y-ROWS")    # 纵向贴片数量
+
+$XY = $X*$Y
+
+$D = [int](ffprobe -v 16 -show_entries format=duration -of csv=p=0 $video)    # 获取视频时长
+
+
+for ($E=1; $E -le $XY; $E++) 
 {
-    $ss = $E*$duration/($number+1)
+    $ss = $E*$D/($XY+1)
 
     $T = "{0:D3}" -f $E
 
-    if ($video.Contains('.hdr.') -or $video.Contains('.HDR.'))
+    if ($video.Contains('.hdr.') -or $video.Contains('.HDR.'))    # 根据文件名判断HDR视频
     {
-        ffmpeg -y -stats -v 16 -ss $ss -i $video -vf zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv -frames:v 1 -pred 2 "F:\Capture_$T.png"
+        ffmpeg -y -stats -v 16 -ss $ss -i $video -vf zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv -frames:v 1 -pred 2 F:\Capture_$T.png
     }
     else
     {
-        ffmpeg -y -stats -v 16 -ss $ss -i $video -frames:v 1 -pred 2 "F:\Capture_$T.png"
+        ffmpeg -y -stats -v 16 -ss $ss -i $video -frames:v 1 -pred 2 F:\Capture_$T.png
     }
 
-    ffmpeg -y -stats -v 16 -i "F:\Capture_$T.png" -q 2 -pix_fmt yuvj420p "F:\Capture_$T.jpg"
+
+    ffmpeg -y -stats -v 16 -i F:\Capture_$T.png -q 2 -pix_fmt yuvj420p F:\Capture_$T.jpg    # PNG 转 JPG（质量91）
+
 }
+
+
+ffmpeg -y -stats -v 16 -i F:\Capture_%3d.png -vf tile=$($X)x$($Y):padding=8,scale=$($W):-2  -q 2 -pix_fmt yuvj420p F:\TILES.jpg    # 可调整贴片边距padding，默认8px
+
 
 Del F:\Capture_*.png -Confirm
 Del F:\Capture_*.jpg -Confirm
+Del F:\TILES.jpg -Confirm
 
-
-
-
-
-
-
-# ffmpeg -y -v 16 -i Capture_%d.png -vf scale=1920:-2,tile=1x5:padding=32 otpt.png
