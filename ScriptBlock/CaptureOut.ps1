b0cb1abb5c157bb2f2b2a8f6737db1f637ae677d
antatura <47680985+antatura@users.noreply.github.com>
2024-07-08 03:20:52 +0800

    ............ -colorspace bt709 -i ..........

--- ScriptBlock/CaptureOut.ps1
+++ ScriptBlock/CaptureOut.ps1
@@ -49,7 +49,6 @@ if ($Frame_info -match 'Dolby Vision RPU Data')
 
 $HDR_Filter = "zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=bgr24"
 $DoVi_Filter = "hwupload,tonemap_opencl=tonemap=bt2390:desat=0:peak=100:format=nv12,hwdownload,format=nv12"
-#(deprecated)  $SDR2PNG_Filter = "zscale=min=709:rin=limited,format=gbrp,format=bgr24"
 $vf = "-vf"
 
 
@@ -77,6 +76,8 @@ for ($A=1; $A -le $XY; $A++)
     elseif ($Timestamp)
     {
         $Filters = $TEXT_Filter
+        $Colorspace = '-colorspace'
+        $BT709 = 'bt709'
     }
     elseif ($Frame_info -match 'bt2020')
     {
@@ -91,8 +92,10 @@ for ($A=1; $A -le $XY; $A++)
     else
     {
         Clear-Variable vf
+        $Colorspace = '-colorspace'
+        $BT709 = 'bt709'
     }
-    ffmpeg -y -v 16 $HW $Opencl -ss $SS -i $Video_FullName -frames:v 1 $vf $Filters -sws_flags accurate_rnd+full_chroma_int+bitexact $env:TEMP\$ASCII'__'$D3.bmp
+    ffmpeg -y -v 16 $Colorspace $BT709 $HW $Opencl -ss $SS -i $Video_FullName -frames:v 1 $vf $Filters -sws_flags accurate_rnd+full_chroma_int+bitexact $env:TEMP\$ASCII'__'$D3.bmp
     
 
 
